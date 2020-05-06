3d060972af699c8fb0e5dcd81a8ad40c8cea03e6
Michał Kopydłowski <m.kopydlowski@merixstudio.com>
2020-05-07 01:17:55 +0200

    Tuesday.

--- src/challenges/admin.py
+++ src/challenges/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
+from .models import Comment
 from .models import Activity
 from .models import Tag
 from .models import Challenge
 
+admin.site.register(Comment)
 admin.site.register(Activity)
 admin.site.register(Tag)
 admin.site.register(Challenge)
--- src/challenges/models.py
+++ src/challenges/models.py
@@ -1,11 +1,24 @@
+from django.contrib.contenttypes.fields import GenericRelation, GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils import timezone
 
 
+
+class Comment(models.Model):
+    body = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True)
+    object_id = models.PositiveIntegerField(null=True)
+    content_object = GenericForeignKey()
+
+    def __str__(self):
+        return self.body
+
 class Activity(models.Model):
     name = models.CharField(max_length=64)
-    image = models.ImageField()
+    #image = models.ImageField()
     factor = models.DecimalField(max_digits=5, decimal_places=2)
+    comments = GenericRelation(Comment)
 
     class Meta:
         ordering = ['name']
@@ -31,6 +44,7 @@ class Challenge(models.Model):
     tags = models.ManyToManyField(Tag, blank=True)
     created = models.DateTimeField(default=timezone.now)
     modified = models.DateTimeField(auto_now_add=True)
+    comments = GenericRelation(Comment)
 
     class Meta:
         ordering = ['name']
